16040b7c6c548c5613a0fa0d96309abbacd6c2e1
Advitiya Brijesh <advitiyabrijesh@Advitiyas-MacBook-Air.local>
2024-02-11 20:29:15 +0530

    Modify lua to sort on the basis of driver distance.

--- play-with-redis-lua/drivers.lua
+++ play-with-redis-lua/drivers.lua
@@ -1,5 +1,11 @@
+-- Sample: redis-cli eval "$(cat drivers.lua)" 2 driver_locations location_info 17.484710933268207 78.36023090530539 6
+
 local drivers = redis.call('georadius', KEYS[1], ARGV[1], ARGV[2], 5, 'km', 'WITHCOORD', 'WITHDIST')
 local matchedDrivers = {}
+
+local function compare(a, b)
+    return tonumber(a[2]) < tonumber(b[2])
+end
 
 for i, driver in ipairs(drivers) do
     local driverKey = 'location_info:' .. driver[1]
@@ -11,5 +17,7 @@ for i, driver in ipairs(drivers) do
         end
     end
 end
+
+table.sort(matchedDrivers, compare)
 
 return matchedDrivers
